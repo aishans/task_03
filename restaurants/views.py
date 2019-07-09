4dd4002b5213d19d2c7052fb75562f17673cff97
Alia <Alia@Ys-MacBook-Pro.local>
2019-07-09 20:37:06 +0300

    task3

--- restaurants/views.py
+++ restaurants/views.py
@@ -5,7 +5,24 @@ def welcome(request):
 
 def restaurant_list(request):
 
-    context = {
+    context = { 
+    	"my_list" : [
+    		#restaurant1:
+    		{
+    		"Restaurant_name": "Five Guys",
+    		"Food_Type": "American cuisine"
+    		},
+    		#restaurant1:
+    		{
+    		"Restaurant_name": "Ora",
+    		"Food_Type": "japanese "
+    		},
+    		#restaurant1:
+    		{
+    		"Restaurant_name": "Burger King",
+    		"Food_Type": "fast food"
+    		}
+    	]
 
     }
     return render(request, 'list.html', context)
@@ -13,7 +30,17 @@ def restaurant_list(request):
 
 def restaurant_detail(request):
 
-    context = {
+    context =   { 
+    	
+   
+    		"my_object":
+    		{
+    			"Restaurant_name": "Tatami",
+    			"food_Type": "japanese"
+    		}
 
-    }
+
+    	}
+
+    
     return render(request, 'detail.html', context)
